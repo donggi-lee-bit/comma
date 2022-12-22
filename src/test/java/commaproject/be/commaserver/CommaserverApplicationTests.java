8fd3ff2c6b2c7ded2cb785f45bf1e769719989b9
donggi-lee-bit <devdonggilee@gmail.com>
2022-12-22 13:13:54 +0900

    init: 프로젝트 빌드에 필요한 파일 추가

---
+++ src/test/java/commaproject/be/commaserver/CommaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package commaproject.be.commaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
