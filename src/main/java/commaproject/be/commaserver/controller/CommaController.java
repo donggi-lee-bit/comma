b3d41067794ae7e998804c2eb0eb0af1181a4bb4
donggi-lee-bit <devdonggilee@gmail.com>
2022-12-28 20:44:19 +0900

    feat: 전체 회고 조회 API 문서 adoc 파일 생성

--- src/main/java/commaproject/be/commaserver/controller/CommaController.java
+++ src/main/java/commaproject/be/commaserver/controller/CommaController.java
@@ -1,8 +1,9 @@
 package commaproject.be.commaserver.controller;
 
 import commaproject.be.commaserver.common.BaseResponse;
-import commaproject.be.commaserver.service.dto.CommaDetailResponse;
 import commaproject.be.commaserver.service.CommaService;
+import commaproject.be.commaserver.service.dto.CommaDetailResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,5 +19,11 @@ public class CommaController {
     public BaseResponse<CommaDetailResponse> readOne(@PathVariable Long commaId) {
         CommaDetailResponse commaDetailResponse = commaService.readOne(commaId);
         return new BaseResponse<>("200", "OK", commaDetailResponse);
+    }
+
+    @GetMapping("/api/commas")
+    public BaseResponse<List<CommaDetailResponse>> readAll() {
+        List<CommaDetailResponse> commaDetailResponses = commaService.readAll();
+        return new BaseResponse<>("200", "OK", commaDetailResponses);
     }
 }
