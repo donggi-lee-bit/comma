933ed48b75627b1571cbb37d1b579ecb183f4e1f
donggi-lee-bit <devdonggilee@gmail.com>
2023-02-10 12:49:18 +0900

    fix: origin 에 올라가지 않는 auth include에서 제거, propertysource 어노테이션 제거

--- src/main/java/commaproject/be/commaserver/common/config/JwtConfig.java
+++ src/main/java/commaproject/be/commaserver/common/config/JwtConfig.java
@@ -4,12 +4,10 @@ import commaproject.be.commaserver.service.dto.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(JwtProperties.class)
-@PropertySource(value = "classpath:application-auth.yml")
 public class JwtConfig {
 
     private final JwtProperties jwtProperties;
