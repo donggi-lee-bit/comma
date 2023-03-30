caeb65d144b73f51b0a42fe28676fc05470fb83e
donggi-lee-bit <devdonggilee@gmail.com>
2023-03-30 15:43:15 +0900

    refactor: 로컬 환경에서 프로필 local 이 실행되도록 변경
    - 프로필 동작을 잘못 이해하고 있었음
      - 기존에는 로컬 환경에서 spring 을 실행할 때 local, test, prod 전부 실행
      - local 만 명시함으로써 로컬 환경에서 local 프로필만 실행하도록 변경
      - test는 ActiveProfiles 어노테이션으로 실행
      - prod 는 docker file 에서 ㅁctive=prod 로 실행

--- src/test/java/commaproject/be/commaserver/CommaserverApplicationTests.java
+++ src/test/java/commaproject/be/commaserver/CommaserverApplicationTests.java
@@ -2,8 +2,10 @@ package commaproject.be.commaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommaserverApplicationTests {
 
     @Test
